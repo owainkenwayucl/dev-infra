d481dabde7bdf49d90c5352098b786cd69681434
Owain Kenway <o.kenway@ucl.ac.uk>
2024-09-19 09:43:25 +0100

    Start ubquity again

--- ubiquity-cnc-environment/outputs.tf
+++
@@ -1,7 +0,0 @@
-output vm_ips {
-  value = harvester_virtualmachine.vm[*].network_interface[0].ip_address
-}
-
-output vm_ids {
-  value = harvester_virtualmachine.vm.*.id
-}
--- ubiquity-cnc-environment/providers.tf
+++
@@ -1,2 +0,0 @@
-provider "harvester" {}
-provider "random" {}
